33cdf549a695a6d60b9d214a81c69f9eaf810553
Zack Shrout <zshrout@gmail.com>
2023-10-05 15:30:38 -0500

    Expanded aura effect actor to also be able to have a duration gameplay effect class.

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -30,5 +30,3 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 	const FGameplayEffectSpecHandle EffectSpecHandle{ TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle) };
 	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
-
- 
