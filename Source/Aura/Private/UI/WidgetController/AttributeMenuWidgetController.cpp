99943d97abcf09b92fd5b031f790dab6beebb179
Zack Shrout <zshrout@gmail.com>
2023-11-14 10:31:22 -0600

    Bound callbacks in attribute menu button row, and have all primary attribute rows showing the name and value of Strength

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,12 +3,22 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+#include "AuraGameplayTags.h"
+
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
-	
+	UAuraAttributeSet* AS{ CastChecked<UAuraAttributeSet>(AttributeSet) };
+
+	check(AttributeInfo);
+
+	FAuraAttributeInfo Info{ AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength) };
+	Info.AttributeValue = AS->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
 }
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
-	GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Orange, FString("Attribute Menu Widget Controller Constructed"));
+	
 }
