48c914da0cc0e355cdf45e57f7cb3b9bc8e3896a
Zack Shrout <zshrout@gmail.com>
2023-11-29 16:19:44 -0500

    Created SendMouseCursorData function to send target data from under cursor to server from client

--- Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
@@ -3,3 +3,35 @@
 
 #include "AbilitySystem/AbilityTask/TargetDataUnderMouse.h"
 
+#include "AbilitySystemComponent.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	return NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	if (Ability->GetCurrentActorInfo()->IsLocallyControlled())
+		SendMouseCursorData();
+	else
+	{
+		// TODO: We are on the server, so listen for target data
+	}
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	FScopedPredictionWindow ScopedPrediction{ AbilitySystemComponent.Get() };
+	
+	FGameplayAbilityTargetData_SingleTargetHit* Data{ new FGameplayAbilityTargetData_SingleTargetHit() };
+	Ability->GetCurrentActorInfo()->PlayerController.Get()->GetHitResultUnderCursor(
+		ECC_Visibility, false, Data->HitResult);
+	
+	FGameplayAbilityTargetDataHandle Handle;
+	Handle.Add(Data);
+	AbilitySystemComponent->ServerSetReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey(), Handle,
+	                                                      FGameplayTag(), AbilitySystemComponent->ScopedPredictionKey);
+	if (ShouldBroadcastAbilityTaskDelegates())
+		ValidData.Broadcast(Handle);
+}
