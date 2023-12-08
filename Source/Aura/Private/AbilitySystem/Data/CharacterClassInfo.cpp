099683ac1adacc5cb058b53efa376a5e6c77c6e3
Zack Shrout <zshrout@gmail.com>
2023-12-07 23:10:15 -0500

    Began making character classes

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright BunnySoft.
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
