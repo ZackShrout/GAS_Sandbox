accafc253d0fbcb70a0b411eb65e15b688175397
Zack Shrout <zshrout@gmail.com>
2024-08-17 15:41:06 -0400

    Created custom Ability System Globals class

---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemGlobals.h
@@ -0,0 +1,19 @@
+// Copyright BunnySoft.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemGlobals.h"
+#include "AuraAbilitySystemGlobals.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemGlobals : public UAbilitySystemGlobals
+{
+	GENERATED_BODY()
+
+private:
+	virtual FGameplayEffectContext* AllocGameplayEffectContext() const override;
+};
